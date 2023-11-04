c172128fe77ade996ac3ba9d81e499dd8004c1b8
Isaiah LaDuke <iladuke@atsapllc.com>
2023-11-04 15:30:37 -0400

    reordering of files and folders

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+import styles from './YourComponent.module.css';
+import { Navbar, Nav, NavItem, NavDropdown } from 'react-bootstrap';
+
+class Header extends React.Component {
+
+    
+    render() {
+        <navbar>
+            <div>
+                <link to="/Home/Home">
+                <p>Home</p>
+                </link>
+                <link to="/Home/Home">
+                <p>About</p>
+                </link>
+                <link to="/Notes/Notes">
+                <p>Notes</p>
+                </link>
+
+
+            </div>
+        </navbar>
+    }
+}

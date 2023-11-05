5806bae28c83a4230e2b1225e00c785f30316140
Sodi Kroehler <sodikroehler@outlook.com>
2023-11-04 21:00:31 -0400

    charlie hedbo

--- src/app/components/theOfficesofcharlieHedbo.tsx
+++ src/app/components/theOfficesofcharlieHedbo.tsx
@@ -4,18 +4,19 @@ import React, {useEffect, useState} from 'react'
 
 const GetUserDetails = () => {
     const [userData, setUserData] = useState([]);
+    const [mytrigger, setTrigger] = useState(false);
 
     useEffect(() => {
         const callAPI = async () => {
-            const query = await fetch('api/user?userID=clokp4tzg0004soz46ntd21uy')
+            const query = await fetch('api/school?userID=clokp4tzg0004soz46ntd21uy')
             const repo = await query.json();
             console.log(repo);
             setUserData(repo.user.name)
         }
         callAPI();
-    }, []);
+    }, [trigger]);
 
-
+    <button onClick= (() => setTrigger(!mytrigger))>
 
         return (
             <div> 

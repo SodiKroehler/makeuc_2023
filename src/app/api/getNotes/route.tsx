74cbff93f364542fd125bab210df4015200b21eb
Sodi Kroehler <sodikroehler@outlook.com>
2023-11-05 06:01:03 -0500

    more redux fixes

--- src/app/api/getNotes/route.tsx
+++ src/app/api/getNotes/route.tsx
@@ -6,12 +6,21 @@ export async function GET(req: NextRequest) {
     const userID =req.nextUrl.searchParams.get("userID")
     const schoolID =req.nextUrl.searchParams.get("schoolID")
     const topicID =req.nextUrl.searchParams.get("topicID")
+    const classID =req.nextUrl.searchParams.get("classID")
+    // let results = [];
   
     if(userID){
-      const listOfClasses = await prismaBugger.class.findMany({
-        where: {schoolID: schoolID??''}
-      })
-  
-      return NextResponse.json({topics: listOfClasses})
+      if(schoolID){
+        if(topicID){
+          if(classID){
+            const results = await prismaBugger.note.findMany({
+              where: {classID: classID}
+            })
+            return NextResponse.json({topics: results})
+          }
+        }
+      }
+
+      
     } else return NextResponse.json({status: 403})
   }
